8edad87248b7edefeaccf8b22e5edc2f126bad6f
harisonde <harisonde@yahoo.co.in>
2018-04-07 20:31:03 -0500

    Update Compaign.sol
    Added approve request and finalize request function to the contract.

--- Compaign.sol
+++ Compaign.sol
@@ -14,6 +14,7 @@ contract Compaign{
     uint public minimumContribution;
     mapping(address => bool) public approvers;
     Request[] public requests;
+    uint approversCount;
     
     modifier onlyManager() {
         require(msg.sender == manager);
@@ -33,6 +34,7 @@ contract Compaign{
     function contribute() payable public {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
+        approversCount++;
     }
     
     function createRequest(string _desc, uint _value, address _reciepint) onlyManager public {
@@ -44,6 +46,22 @@ contract Compaign{
             approvalCount: 0
         });
         requests.push(request);
+    }
+    
+    function approveRequest(uint _requestNo) onlyApprover public{
+        Request storage request = requests[_requestNo];
+        require(request.approvals[msg.sender] == false);
+        request.approvals[msg.sender] = true;
+        request.approvalCount += 1;
+    }
+    
+    function finalizeRequest(uint _requestNo) onlyManager public {
+         Request storage request = requests[_requestNo];
+         require(!request.complete);
+         require(request.approvalCount > (approversCount / 2));
+         
+         request.complete = true;
+         request.reciepint.transfer(request.value);
     }
     
 }
