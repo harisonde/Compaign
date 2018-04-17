4364e312844cf4d9fccb8ba89c53498f97d5beb1
harisonde <harisonde@yahoo.co.in>
2018-04-17 06:38:47 -0500

    Updated request new page

--- pages/compaigns/requests/new.js
+++ pages/compaigns/requests/new.js
@@ -10,7 +10,8 @@ class RequestNew extends Component{
     value:'',
     description:'',
     reciepint:'',
-    loading:false
+    loading:false,
+    errorMessage:''
   }
   static async getInitialProps(props){
     const address = props.query.address;
@@ -19,19 +20,35 @@ class RequestNew extends Component{
 
   onSubmit = async (event) =>{
     event.preventDefault();
-    this.setState({this.state.loading: true});
+    this.setState(
+      {this.state.loading: true,
+        errorMessage:''
+      });
+    const {description, value, reciepint} = this.state;
+
     try{
       const address = await web3.eth.getAccounts();
-
+      const compaignObj = compaignCreate(this.props.address);
+      await compaignFC.methods.createRequest(
+        description,
+        web3.utils.toWei(value, 'ether'),
+        reciepint).send({from:accounts[0]}
+        );
+        Routes.pushRoute(`/compaigns/${this.props.address}/requests`);
     }catch(error){
-
+    this.setState({
+      errorMessage: error.message
+    });
     }
     this.setState({this.state.loading: false});
   }
   render(){
     return(
       <Layout>
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+        <Link route=`/compaigns/${this.props.address}/requests`>
+          <a>Back</a>
+        </Link>
       <h3>Create a Request!</h3>
         <Form.Field>
           <label>Description</label>
@@ -58,6 +75,7 @@ class RequestNew extends Component{
         </Form.Field>
 
         <Form.Field>
+          <Message error header="Oops" content={this.state.errorMessage}/>
           <Button loading={this.state.loading} primary>Create</Button>
         </Form.Field>
       </Form>
